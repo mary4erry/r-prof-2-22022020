07dd1aed58978c7dceab78cc268b9dcbf8bf9970
Maria Vatolina <mary4erry@yandex.ru>
2020-04-17 01:00:29 +0300

    clear folder

--- Students/Maria Vatolina/Project5/src/store/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import  msgReducer from './messages_reducer.js'
-import  chatReducer from './chats_reducer.js'
-
-export default combineReducers({ msgReducer, chatReducer })
