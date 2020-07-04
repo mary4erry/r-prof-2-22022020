9947c27a85a5f9bcc27f795f26817df7d08b2c26
Maria Vatolina <mary4erry@yandex.ru>
2020-07-04 21:31:42 +0300

    delet

--- Students/Maria Vatolina/social-net/src/HOC/WithAuthRedirect.js
+++ Students/Maria Vatolina/social-net/src/HOC/WithAuthRedirect.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactComponent } from 'react';
+// import { ReactComponent } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
