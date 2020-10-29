a9022610f095bab7b018b81196048a66d349cd1a
Maria Vatolina <mary4erry@yandex.ru>
2020-10-29 21:27:55 +0300

    1_add Components: Message, MessageField

---
+++ Students/Maria Vatolina/ReactGram/src/Components/Message.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const MessageComponent = (props) => {
+   return <div>
+      {props.text}
+   </div> 
+}
+export default MessageComponent
---
+++ Students/Maria Vatolina/ReactGram/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import MessageField from './Components/MessageField.jsx'
+
+
+ReactDOM.render(
+   <MessageField />,
+   document.getElementById('app'),
+)
