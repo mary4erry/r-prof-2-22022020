a390ad8dac8b60001e1ca4cd6e51e3a82b1b49d2
Maria Vatolina <mary4erry@yandex.ru>
2021-03-06 13:53:25 +0300

    fix styles MU

--- Students/Maria Vatolina/ReactGram/src/Components/Message/Message.jsx
+++ Students/Maria Vatolina/ReactGram/src/Components/Message/Message.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './Message.css'
+import { withStyles } from '@material-ui/core/styles'
+import { Box } from '@material-ui/core'
+
+const useStyles = (theme => ({
+   root: {
+      display: 'flex',
+      flexDirection: 'column',
+      color: theme.palette.text.primary,
+   },
+   msgAnswer: {
+      backgroundColor: 'skyblue',
+      alignSelf: 'flex-start',
+   },
+   msgMy: {
+      backgroundColor: 'lightblue',
+      alignSelf: 'flex-end',
+   },
+   msgSender:{
+      // fontSize: '12px',
+      color: 'lightslategray',
+   },
+   msgText:{
+      margin:'5px',
+   }
+}))
 
 const Message = (props) => {
-   let { sender, text, chatId, chats } = props
+   let { sender, text, chatId, chats, classes } = props
    sender ? sender = sender : sender = chats[chatId].title
 
-   let msgClass = 'msg'
-   sender !== 'Me' ? msgClass += ' ' + 'msgAnswer' : 
-      msgClass += ' ' +  'msgMy'
+   let msgClass = (sender !== 'Me' ? classes.msgAnswer : classes.msgMy)
+   
    return (
-      <div className={msgClass}>
-         <strong className='msgSender'> { sender }: </strong>
-         <p className='msgText'>{ props.sender || (!props.sender && text) 
+      <div className={classes.root}>
+         <Box className={msgClass}  m={1} p={1} width="40%" borderRadius="10px" boxShadow={1}>
+         <strong className={classes.msgSender}> { sender }: </strong>
+         <p className={classes.msgText}>{ props.sender || (!props.sender && text) 
             ? text : 'go away...' }
          </p>
-      </div> 
+      </Box> 
+      </div>
+      
    )
 }
-export default Message
+export default withStyles(useStyles)(Message)
